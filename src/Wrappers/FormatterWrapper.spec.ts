d57212302e40a2662ee9a4073113724a49329380
arthus2 <shane.arthur@td.com>
2017-01-12 17:23:57 -0500

    Intermitten checkin broken tests.

---
+++ src/Wrappers/FormatterWrapper.spec.ts
@@ -0,0 +1,9 @@
+import { test } from '../FormatterWrapper';
+
+describe('FormatterWrapper', function () {
+
+    it('should assign properties on construct', function () {
+      var check, gay = null;
+      test.doSomeShit();
+    });
+});
---
+++ src/Wrappers/SampleTest.ts
@@ -0,0 +1,7 @@
+
+
+export const test = {
+  doSomeShit() {
+    console.log('aids');
+  }
+}
