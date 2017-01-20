3cbc1e158a563131dbd5a6ef3b5b147c977657d7
arthus2 <shane.arthur@td.com>
2017-01-20 17:08:14 -0500

    Fixing up formatterWrapper tests.

--- src/Wrappers/FormatterWrapper.spec.ts
+++ src/Wrappers/FormatterWrapper.spec.ts
@@ -1,31 +1,39 @@
 import { styleUtil } from './FormatterWrapper';
 import { OffsetObject } from '../interfaces/PixelObject';
 
-describe('FormatterWrapper', () => {
-    it('should correctly add two values', () => {
-        const valuesToTest: string[] = ['left: 10px', 'left: 120px'];
 
-        let horizontalChange: OffsetObject = null;
-        let verticalChange: OffsetObject = null;
-        [horizontalChange, verticalChange] = styleUtil.addPixels(valuesToTest)
+describe('FormatterWrapper', () => {
+    let valuesToTest: string[];
+    let horizontalChange: OffsetObject;
+    let verticalChange: OffsetObject;
+
+    beforeEach(() => {
+        valuesToTest = null;
+        horizontalChange = null;
+        verticalChange = null;
+    })
+
+    const performAddition = () => {
+        [horizontalChange, verticalChange] = styleUtil.addPixels(valuesToTest);
+    }
+
+    it('should correctly add two values', () => {
+        valuesToTest = ['left: 10px', 'left: 120px'];
+        performAddition();
         expect(horizontalChange.offsetValue).toBe(130);
         expect(verticalChange.offsetValue).toBe(0);
     });
 
     it('should disregard direction values not l/r/t/b', () => {
-        const valuesToTest: string[] = ['leftBull: 10px', 'left: 120px'];
-        let horizontalChange: OffsetObject = null;
-        let verticalChange: OffsetObject = null;
-        [horizontalChange, verticalChange] = styleUtil.addPixels(valuesToTest);
+        valuesToTest= ['leftBull: 10px', 'left: 120px'];
+        performAddition();
         expect(horizontalChange.offsetValue).toBe(120);
         expect(verticalChange.offsetValue).toBe(0);
     });
 
     it('should add both vertical and horizontal values', () => {
-        const valuesToTest: string[] = ['left : 100px', 'right: 200px', 'top: 100px', 'bottom: 50px'];
-        let horizontalChange: OffsetObject = null;
-        let verticalChange: OffsetObject = null;
-        [horizontalChange, verticalChange] = styleUtil.addPixels(valuesToTest);
+        valuesToTest = ['left : 100px', 'right: 200px', 'top: 100px', 'bottom: 50px'];
+        performAddition();
         expect(horizontalChange.offsetValue).toBe(-100);
         expect(verticalChange.offsetValue).toBe(50);
     });
